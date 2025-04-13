924754eb2d8c84029775853029c41af8069f638c
jelyang143 <jutba.jhella.143@gmail.com>
2025-04-14 01:14:03 +0800

    Update frontend URL in CORS configuration

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 origins = [
     "http://localhost:5173",
-    "https://rodillon-todo-reactvite.netlify.app/",
+    "https://symphonious-bombolone-c96681.netlify.app/",
 ]
 
 app.add_middleware(
